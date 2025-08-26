8e561a26bedd6d8fd88c88867a42a1f2a86b9df4
Essentials <etienomaurice15@gmail.com>
2025-08-26 10:18:50 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+    function openNav() {
+        let sidebar = document.getElementById("sidebar");
+        sidebar.style.display = 'flex'
+}
+    function closeNav() {
+        let sidebar = document.getElementById("sidebar");
+        sidebar.style.display = 'none'
+}
